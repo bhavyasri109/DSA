0ea18ce2cabcc686090cd9444fec883bd34c0030
bhavyasri109 <gangiguntlabhavyasri@gmail.com>
2026-01-07 09:34:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (85.21%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int maxx =nums[0];
-        int minn =nums[0];
-        int sol  =nums[0];
-        for (int i=1;i<nums.size();i++) {
-            if (nums[i] < 0)
+        int maxx=nums[0];
+        int minn=nums[0];
+        int sol=nums[0];
+        for(int i=1;i<nums.size();i++) {
+            if(nums[i]<0)
                 swap(maxx, minn);
-            maxx =max(nums[i],nums[i]*maxx);
-            minn =min(nums[i],nums[i]*minn);
-            sol =max(sol,maxx);
+            maxx=max(nums[i],nums[i]*maxx);
+            minn=min(nums[i],nums[i]*minn);
+            sol=max(sol,maxx);
         }
         return sol;
     }
