3c66cf2828804cbfa4f2dece484a0de0baa5903d
bhavyasri109 <gangiguntlabhavyasri@gmail.com>
2026-03-24 14:57:02 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (66.78%) - LeetSync

---
+++ 65-valid-number/valid-number.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isNumber(String s) {
+        s = s.trim();
+        boolean numSeen = false;
+        boolean dotSeen = false;
+        boolean eSeen = false;
+        boolean numAfterE = true;
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c >= '0' && c <= '9'){
+                numSeen = true;
+                numAfterE = true;
+            }
+            else if(c == '.'){
+                if(dotSeen || eSeen) return false;
+                dotSeen = true;
+            }
+            else if(c == 'e' || c == 'E'){
+                if(eSeen || !numSeen) return false;
+                eSeen = true;
+                numAfterE = false;
+            }
+            else if(c == '+' || c == '-'){
+                if(i != 0 && s.charAt(i-1) != 'e' && s.charAt(i-1) != 'E'){
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        return numSeen && numAfterE;
+    }
+}
