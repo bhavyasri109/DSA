aec046fdc33f6601d78063e89442a77bfee8af83
bhavyasri109 <gangiguntlabhavyasri@gmail.com>
2026-07-25 12:17:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (92.60%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,17 +1,19 @@
 class Solution:
+    def robber(self,nums):
+        n=len(nums)
+        if n==1:
+            return nums[0]
+        dp=[0]*n
+        dp[0]=nums[0]
+        dp[1]=max(nums[0],nums[1])
+        for i in range(2,n):
+            t=nums[i]+dp[i-2]
+            s=dp[i-1]
+            dp[i]=max(t,s)
+        return dp[n-1]
     def rob(self, nums: List[int]) -> int:
         n=len(nums)
         if n==1:
             return nums[0]
-        def solve(arr):
-            @cache
-            def f(i):
-                if i<0:
-                    return 0
-                if i==0:
-                    return arr[0]
-                p=arr[i]+f(i-2)
-                np=f(i-1)
-                return max(p,np)
-            return f(len(arr)-1)
-        return max(solve(nums[:-1]),solve(nums[1:]))
+        return max(self.robber(nums[:-1]),self.robber(nums[1:]))
+    
