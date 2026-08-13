976dfe53bfb6dc5a7b6ff27930809af649ad502c
bhavyasri109 <gangiguntlabhavyasri@gmail.com>
2026-08-13 14:25:42 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (42.59%) - LeetSync

---
+++ 2556-convert-the-temperature/convert-the-temperature.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public double[] ConvertTemperature(double celsius) {
+        double k=celsius+273.15;
+        double f=celsius*1.80+32.00;
+        return new double[]{k,f};
+    }
+}
